e3ddb19ce8cd099b1e10cbecacb9a6e0b494e238
samantha lin <samanthalin1992@gmail.com>
2021-07-11 01:01:49 +0800

    header, footer, rwd

--- library/js/load_content.js
+++ library/js/load_content.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
-	var pageID = $('body').attr('id');
-	if (pageID.toString() == 'page_homepage'){
-		$('head').load('library/inc/home_head.inc');
-		$('.mobile-menu').load('library/inc/mobile_menu.inc');
-		$('header').load('library/inc/header.inc');
-		$('footer').load('library/inc/footer.inc');
-	}else{
-		$('head').load('../library/inc/head.inc');
-		$('.mobile-menu').load('../library/inc/mobile_menu.inc');
-		$('header').load('../library/inc/header.inc');
-		$('footer').load('../library/inc/footer.inc');
-	}
+	$('head').load('../library/inc/head.inc');
+	$('.mobile-menu').load('../library/inc/mobile_menu.inc');
+	$('header').load('../library/inc/header.inc');
+	$('footer').load('../library/inc/footer.inc');
 })
--- library/js/style.js
+++ library/js/style.js
@@ -1,17 +1,36 @@
 $(document).ready(function(){
-	
+
 	//header
 	$('.hamburger').click(function(){
 		$('.body-wrap').toggleClass('menu-open');
 		$(this).toggleClass('active');
-		$('.mobile-menu').toggleClass('active');
 	});
 	$('.social').click(function(){
 		$(this).toggleClass('active');
 		var left = $(this).position().left;
-		$('.social-menu').toggleClass('active');
-		$('.social-menu').css('left', left);
+		$('.mobile-social-menu').toggleClass('active');
+		$('.mobile-social-menu').css('left', left);
 	});
-	$('.social-menu a').attr('target', '_blank');
+	$('.mobile-social-menu a').attr('target', '_blank');
+	$('.desktop-social-menu a').attr('target', '_blank');
+	
+	var pageID = $('body').attr('id');
+	$('.mobile-menu ul li a, .menu-bar ul li a').each(function(){
+		var pageTarget = $(this).attr('page-target');
+		if (pageTarget == pageID){
+			$(this).addClass('current-page');
+		}
+	});
 
+	$(window).resize(function(){
+		$('.hamburger').removeClass('active');
+		$('.body-wrap').removeClass('menu-open');
+		$('.social').removeClass('active');
+		$('.mobile-social-menu').removeClass('active');
+	});
+
+	//footer
+	$('footer a').attr('target','_blank');
+
+	//homepage
 });
