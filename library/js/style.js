e9444239dfe605ec8ddaafb65d11c64670023dfd
samantha lin <samanthalin1992@gmail.com>
2021-07-16 01:21:50 +0800

    programme done

--- library/js/style.js
+++ library/js/style.js
@@ -65,4 +65,17 @@ $(document).ready(function(){
 		var selectSection = $(this).attr('href');
 		$('html,body').animate({scrollTop: $(selectSection).offset().top},'slow');
 	});
+	$('#page_programme .movie button').click(function(){
+		$('body').addClass('popup-show');
+		$('#page_programme .popup').css('display','flex');
+		$('#page_programme .popup .popup-content').addClass('animated');
+		var contentID = $(this).attr('data-target');
+		var target = '../library/inc/popup_content.inc ' + contentID;
+		console.log(target);
+		$('.popup .popup-content').load(target);
+	});
+	$('#page_programme .popup .close-btn').click(function(){
+		$('body').removeClass('popup-show');
+		$('#page_programme .popup').hide();
+	});
 });
