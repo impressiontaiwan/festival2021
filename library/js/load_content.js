d7a9fc7b1cc8ff4f1ad88218b66d48b641a788bf
samantha lin <samanthalin1992@gmail.com>
2021-07-22 19:17:29 +0800

    html, js order adjust

--- library/js/load_content.js
+++ library/js/load_content.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-	$('head').load('../library/inc/head.inc');
 	$('.mobile-menu').load('../library/inc/mobile_menu.inc');
 	$('header').load('../library/inc/header.inc');
 	$('footer').load('../library/inc/footer.inc');
+	$('head').load('../library/inc/head.inc');
 })
